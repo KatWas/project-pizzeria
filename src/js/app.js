2d1b39aef978c10225592966f18392c84501b9b6
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-10-21 11:35:48 +0200

    add changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,122 +1,122 @@
- import {settings, select, classNames} from './settings.js';
- import Product from './components/Product.js';
- import Cart from './components/Cart.js';
- import Booking from './components/booking.js';
+import {settings, select, classNames} from './settings.js';
+import Product from './components/Product.js';
+import Cart from './components/Cart.js';
+import Booking from './components/booking.js';
 
 
- const app = {
+const app = {
 
     
-    initPages:function(){
-        const thisApp = this;
+  initPages:function(){
+    const thisApp = this;
 
-        thisApp.pages = document.querySelector(select.containerOf.pages).children;
-        thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
         
-        const idFromHash = window.location.hash.replace ('#/', '');
-        console.log('idFromHash', idFromHash);
+    const idFromHash = window.location.hash.replace ('#/', '');
+    console.log('idFromHash', idFromHash);
 
-        let pageMatchingHash = thisApp.pages[0].id;
+    let pageMatchingHash = thisApp.pages[0].id;
         
-        for(let page of thisApp.pages){
-          if(page.id == idFromHash){
-              pageMatchingHash = page.id;
-              break;
-          }
-        }
-        
-        thisApp.activatePage(pageMatchingHash);
-
-        for (let link of thisApp.navLinks){
-          link.addEventListener('click', function (event){
-            const clickedElement = this;
-            event.preventDefault();
-
-            /* get page id from href attribute*/
-
-            const id = clickedElement.getAttribute('href').replace("#", '');
-
-            /* run thisApp.activatePage with that id */
-            thisApp.activatePage(id);
-
-            /* change URL hash */
-            window.location.hash = '#/' + id;
-          });
-        }
-        
-    },
-    activatePage: function (pageId){
-      const thisApp = this;
-
-      /* add class "active" to matching pages, remove from non-matching*/
-      for (let page of thisApp.pages){
-         page.classList.toggle(classNames.pages.active, page.id == pageId);
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
       }
-      /* add class "active" to matching links, remove from non-matching*/
-      for (let link of thisApp.navLinks){
-        link.classList.toggle(
-          classNames.nav.active, 
-          link.getAttribute ('href') == '#' + pageId
-          );
-     }},
+    }
+        
+    thisApp.activatePage(pageMatchingHash);
 
-    initMenu: function () {
-      const thisApp = this;
-      // console.log('this App', thisApp);
-      for (let productData in thisApp.data.products) {
-        new Product(
-          thisApp.data.products[productData].id,
-          thisApp.data.products[productData]
-        );
-      }
-    },
+    for (let link of thisApp.navLinks){
+      link.addEventListener('click', function (event){
+        const clickedElement = this;
+        event.preventDefault();
 
-    initData: function () {
-      const thisApp = this;
+        /* get page id from href attribute*/
 
-      thisApp.data = {};
-      const url = settings.db.url + '/' + settings.db.products;
+        const id = clickedElement.getAttribute('href').replace('#', '');
 
-      fetch(url)
-        .then(function (rawResponse) {
-          return rawResponse.json();
-        })
-        .then(function (parsedResponse) {
-          //console.log('parsed Response', parsedResponse);
-          thisApp.data.products = parsedResponse;
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
 
-          thisApp.initMenu();
-        });
-      //console.log('thisapp.data', JSON.stringify(thisApp.data));
-    },
-
-    init: function () {
-      const thisApp = this;
-      // console.log('*** App starting ***');
-      // console.log('thisApp:', thisApp);
-      // console.log('classNames:', classNames);
-      // console.log('settings:', settings);
-      // console.log('templates:', templates);
-
-      thisApp.initPages();
-      thisApp.initBooking();
-
-      thisApp.initData();
-      thisApp.initCart();
-    },
-
-    initCart: function () {
-      const thisApp = this;
-
-      const cartElem = document.querySelector(select.containerOf.cart);
-      thisApp.cart = new Cart(cartElem);
-
-      thisApp.productsList = document.querySelector(select.containerOf.menu);
-    
-      thisApp.productsList.addEventListener('add-to-cart', function(event){
-        app.cart.add(event.detail.product);
+        /* change URL hash */
+        window.location.hash = '#/' + id;
       });
-    },
+    }
+        
+  },
+  activatePage: function (pageId){
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching*/
+    for (let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    /* add class "active" to matching links, remove from non-matching*/
+    for (let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute ('href') == '#' + pageId
+      );
+    }},
+
+  initMenu: function () {
+    const thisApp = this;
+    // console.log('this App', thisApp);
+    for (let productData in thisApp.data.products) {
+      new Product(
+        thisApp.data.products[productData].id,
+        thisApp.data.products[productData]
+      );
+    }
+  },
+
+  initData: function () {
+    const thisApp = this;
+
+    thisApp.data = {};
+    const url = settings.db.url + '/' + settings.db.products;
+
+    fetch(url)
+      .then(function (rawResponse) {
+        return rawResponse.json();
+      })
+      .then(function (parsedResponse) {
+        //console.log('parsed Response', parsedResponse);
+        thisApp.data.products = parsedResponse;
+
+        thisApp.initMenu();
+      });
+    //console.log('thisapp.data', JSON.stringify(thisApp.data));
+  },
+
+  init: function () {
+    const thisApp = this;
+    // console.log('*** App starting ***');
+    // console.log('thisApp:', thisApp);
+    // console.log('classNames:', classNames);
+    // console.log('settings:', settings);
+    // console.log('templates:', templates);
+
+    thisApp.initPages();
+    thisApp.initBooking();
+
+    thisApp.initData();
+    thisApp.initCart();
+  },
+
+  initCart: function () {
+    const thisApp = this;
+
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
+
+    thisApp.productsList = document.querySelector(select.containerOf.menu);
+    
+    thisApp.productsList.addEventListener('add-to-cart', function(event){
+      app.cart.add(event.detail.product);
+    });
+  },
 
   initBooking: function(){
     const thisApp = this;
@@ -125,4 +125,4 @@
     thisApp.booking= new Booking(bookingContainer);
   },
 };
-  app.init();
+app.init();
--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,37 +1,38 @@
 import { settings, select} from '../settings.js';
+
 class AmountWidget {
-    constructor(element) {
-      const thisWidget = this;
-      thisWidget.getElements(element);
-      //console.log('AmountWidget:', thisWidget);
-      //console.log('consrucrot argument:', element);
-      thisWidget.setValue(
-        thisWidget.input.value || settings.amountWidget.defaultValue
-      );
-      thisWidget.initActions();
-    }
+  constructor(element) {
+    const thisWidget = this;
+    thisWidget.getElements(element);
+    //console.log('AmountWidget:', thisWidget);
+    //console.log('consrucrot argument:', element);
+    thisWidget.setValue(
+      thisWidget.input.value || settings.amountWidget.defaultValue
+    );
+    thisWidget.initActions();
+  }
 
-    getElements(element) {
-      const thisWidget = this;
+  getElements(element) {
+    const thisWidget = this;
 
-      thisWidget.element = element;
-      thisWidget.input = thisWidget.element.querySelector(
-        select.widgets.amount.input
-      );
-      thisWidget.linkDecrease = thisWidget.element.querySelector(
-        select.widgets.amount.linkDecrease
-      );
-      thisWidget.linkIncrease = thisWidget.element.querySelector(
-        select.widgets.amount.linkIncrease
-      );
-    }
+    thisWidget.element = element;
+    thisWidget.input = thisWidget.element.querySelector(
+      select.widgets.amount.input
+    );
+    thisWidget.linkDecrease = thisWidget.element.querySelector(
+      select.widgets.amount.linkDecrease
+    );
+    thisWidget.linkIncrease = thisWidget.element.querySelector(
+      select.widgets.amount.linkIncrease
+    );
+  }
 
-    setValue(value) {
-      const thisWidget = this;
+  setValue(value) {
+    const thisWidget = this;
 
-      // console.log(thisWidget.value, 'thisssssssssssssssssss');
+    // console.log(thisWidget.value, 'thisssssssssssssssssss');
 
-      /* czy mogłem to tak zapisac!!!!!!!!!!!!!!!
+    /* czy mogłem to tak zapisac!!!!!!!!!!!!!!!
       const newValue = parseInt(value);
       if (thisWidget.value !== newValue) {
         thisWidget.value == newValue;
@@ -39,44 +40,44 @@ class AmountWidget {
         if (isNaN(newValue)) thisWidget.value == newValue;
       }
       */
-      const newValue = parseInt(value);
-      if (
-        thisWidget.value !== newValue &&
+    const newValue = parseInt(value);
+    if (
+      thisWidget.value !== newValue &&
         !isNaN(newValue) &&
         newValue >= settings.amountWidget.defaultMin &&
         newValue <= settings.amountWidget.defaultMax
-      ) {
-        thisWidget.value = newValue; // on mi ustawia wartoś inpouta
-        // console.log(thisWidget.value, 'this value');
-      }
-      thisWidget.input.value = thisWidget.value;
-      thisWidget.announce();
-      //settings.amountWidget.defaultMax
+    ) {
+      thisWidget.value = newValue; // on mi ustawia wartoś inpouta
+      // console.log(thisWidget.value, 'this value');
     }
-
-    initActions() {
-      const thisWidget = this;
-
-      thisWidget.input.addEventListener('change', function () {
-        thisWidget.setValue(thisWidget.input.value);
-      });
-      thisWidget.linkDecrease.addEventListener('click', function (event) {
-        event.preventDefault();
-        thisWidget.setValue(thisWidget.value - 1);
-      });
-
-      thisWidget.linkIncrease.addEventListener('click', function (event) {
-        event.preventDefault;
-        thisWidget.setValue(thisWidget.value + 1);
-      });
-    }
-
-    announce() {
-      const thisWidget = this;
-      const event = new CustomEvent('updated', {
-        bubbles: true,
-      });
-      thisWidget.element.dispatchEvent(event);
-    }
+    thisWidget.input.value = thisWidget.value;
+    thisWidget.announce();
+    //settings.amountWidget.defaultMax
   }
-  export default AmountWidget;
+
+  initActions() {
+    const thisWidget = this;
+
+    thisWidget.input.addEventListener('change', function () {
+      thisWidget.setValue(thisWidget.input.value);
+    });
+    thisWidget.linkDecrease.addEventListener('click', function (event) {
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value - 1);
+    });
+
+    thisWidget.linkIncrease.addEventListener('click', function (event) {
+      event.preventDefault;
+      thisWidget.setValue(thisWidget.value + 1);
+    });
+  }
+
+  announce() {
+    const thisWidget = this;
+    const event = new CustomEvent('updated', {
+      bubbles: true,
+    });
+    thisWidget.element.dispatchEvent(event);
+  }
+}
+export default AmountWidget;
