f3ee3b2097cd45ed0ba27c465fdb1e8cb43f1b5c
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-10-25 11:17:18 +0200

    te

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,6 @@ import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 import Booking from './components/Booking.js';
-import DatePicker from './components/Classes/DatePicker.js';
-import HourPicker from './components/Classes/HourPicker.js';
-
 
 const app = {
 
